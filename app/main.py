a27d24b4f4f1388b69d1ce90b2b1d88927a9ed4d
Himalaya <himalaya341@gmail.com>
2025-04-21 23:02:38 +0530

    test

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import analytics  # import router
 from app.routes import product_recomendation 
+from app.routes import sentiment
 from app.routes import top_brands
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -9,11 +10,13 @@ app = FastAPI(title="AmazoLens")  # create app instance
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Allow the frontend origin
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+
 @app.get("/")
 def root():
     return {"message": "🚀 AmazoLens backend is live! And yes... I'm working fine 😎"}
@@ -22,4 +25,4 @@ def root():
 app.include_router(analytics.router)  # attach routes
 app.include_router(product_recomendation.router)
 app.include_router(top_brands.router)  # attach routes
-
+app.include_router(sentiment.router)  # attach routes
